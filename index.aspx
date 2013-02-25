bf6ac6927c9d84bb363b166f55f86965fd71e2f2
unknown <cscotton@PBB-C320R-13.iowa.uiowa.edu>
2013-02-25 12:36:31 -0600

    initial commit

---
+++ index.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="index.aspx.vb" Inherits="index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Net Pay Calculator</div>
+    <p>
+        Please enter hourly wage:</p>
+    <p>
+        <asp:TextBox ID="wageTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Please enter hours worked this week:</p>
+    <p>
+        <asp:TextBox ID="hoursTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Please enter any pre-tax deductions:</p>
+    <p>
+        <asp:TextBox ID="preTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Please enter any after-tax deductions:</p>
+    <p>
+        <asp:TextBox ID="afterTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="calculateButton" runat="server" Text="Calculate" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="clearButton" runat="server" Text="Clear" />
+    </p>
+    <p>
+        Employee Net Take Home Pay: 
+        <asp:Label ID="netLabel" runat="server"></asp:Label>
+    </p>
+    <p>
+        &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ index.aspx.vb
@@ -0,0 +1,50 @@
+﻿
+Partial Class index
+    Inherits System.Web.UI.Page
+    Function calculateNet() As Decimal
+        'specify constant tax rate
+        Const taxRate As Decimal = 0.18
+
+        'create variables to hold values of user input
+        Dim wage As Decimal = Val(wageTextBox.Text)
+        Dim hours As Decimal = Val(hoursTextBox.Text)
+        Dim pre As Decimal = Val(preTextBox.Text)
+        Dim after As Decimal = Val(afterTextBox.Text)
+        Dim preResult As Decimal
+        Dim gross As Decimal
+        Dim netResult As Decimal
+        Dim taxTotal As Decimal
+
+        'calculate gross earnings per week
+        gross = wage * hours
+
+        'calculate gross after pre-tax deduction
+        preResult = gross - pre
+
+        'calculate tax due after pre-tax deductions
+        taxTotal = preResult * taxRate
+
+        'calculate net take home pay
+        netResult = gross - pre - taxTotal - after
+
+        Return netResult
+    End Function
+
+    Protected Sub calculateButton_Click(sender As Object, e As System.EventArgs) Handles calculateButton.Click
+        
+
+        'Display Net take home pay after taxes
+        netLabel.Text = calculateNet()
+
+
+    End Sub
+
+    Protected Sub clearButton_Click(sender As Object, e As System.EventArgs) Handles clearButton.Click
+        wageTextBox.Text = ""
+        hoursTextBox.Text = ""
+        preTextBox.Text = ""
+        afterTextBox.Text = ""
+        netLabel.Text = ""
+
+    End Sub
+End Class
